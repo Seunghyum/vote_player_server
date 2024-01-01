e3c2fa82fbffac347a1096e231472e8f68d14b9d
Seunghyum <antmgu@gmail.com>
2024-01-01 13:13:24 +0900

    feat: add filter to candidate find route

--- src/routes/candidates.ts
+++ src/routes/candidates.ts
@@ -3,10 +3,17 @@ import Candidates from "@models/candidates";
 
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  Candidates.find({}).then((candidates) => {
-    res.json(candidates);
-  });
+router.get("/", async (req, res, next) => {
+  const { koName, partyName, pageCount = 15, currentPage = 0 } = req.query;
+  const pc = parseInt(pageCount as string);
+  const cp = parseInt(currentPage as string);
+
+  const result = await Candidates.find()
+    .or([{ koName }, { partyName }])
+    .skip(pc * cp)
+    .limit(pc);
+
+  res.json(result);
 });
 
 router.get("/:id", function (req, res, next) {
