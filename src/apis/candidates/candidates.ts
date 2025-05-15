f63ee8dbdbbe5451bcb0c2f709e9ab9914bd3082
Seunghyum <antmgu@gmail.com>
2025-05-15 15:40:25 +0900

    feat: candidates api 최신화

--- src/apis/candidates/candidates.ts
+++ src/apis/candidates/candidates.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-import getCandidatesSimples from './candidates_simples_api';
+import getCandidatesSimples from "./candidates_simples_api";
 import { getCandidatesDetails } from "./candidates_details_api";
 import candidates from "@models/candidates";
 import chalk from "chalk";
 
 const { MONGO_URI, MONGO_DBNAME, MONGO_USERNAME, MONGO_PASSWORD } = process.env;
-
 
 if (!MONGO_URI) throw Error("dontenv 설정을 확인하세요");
 
@@ -21,11 +20,16 @@ mongoose
   .catch((e) => console.error(e));
 
 export default async function initCandidates() {
-  const simples = await getCandidatesSimples()
-  for(const simple of simples) {
-    const detail = await getCandidatesDetails({dept_cd: simple.deptCd, num: simple.num})
-    await candidates.create({...simple, ...detail})
-    console.log(chalk.blue(`document 추가 - ${await candidates.countDocuments()}`))
+  const simples = await getCandidatesSimples();
+  for (const simple of simples) {
+    const detail = await getCandidatesDetails({
+      dept_cd: simple.deptCd,
+      num: simple.num,
+    });
+    await candidates.create({ ...simple, ...detail });
+    console.log(
+      chalk.blue(`document 추가 - ${await candidates.countDocuments()}`)
+    );
   }
 }
 
--- src/apis/candidates/candidates_details_api.ts
+++ src/apis/candidates/candidates_details_api.ts
@@ -1,10 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 import mongoose from "mongoose";
 import "dotenv/config";
-import candidatesSimple from '@models/candidates_simple';
 
-const { CANDIDATE_API_KEY, MONGO_URI, MONGO_DBNAME, MONGO_USERNAME, MONGO_PASSWORD } = process.env;
-
+const {
+  CANDIDATE_API_KEY,
+  MONGO_URI,
+  MONGO_DBNAME,
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+} = process.env;
 
 if (!MONGO_URI) throw Error("dontenv 설정을 확인하세요");
 
@@ -19,20 +23,29 @@ mongoose
   .catch((e) => console.error(e));
 
 interface getCandidatesSimplesProps {
-  dept_cd: String,
-  num: String
+  dept_cd: String;
+  num: String;
 }
 
-export function getCandidatesDetails({dept_cd, num}:getCandidatesSimplesProps) {
-  return axios.get(`https://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberDetailInfoList`, {
-      params: {
+export function getCandidatesDetails({
+  dept_cd,
+  num,
+}: getCandidatesSimplesProps) {
+  return axios
+    .get(
+      `https://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberDetailInfoList`,
+      {
+        params: {
           dept_cd,
           num,
           serviceKey: CANDIDATE_API_KEY,
           numOfRows: 10,
-          pageNo: 1
-      },
-  }).then(async (res) => {
-    return res.data.response.body.item
-  }).catch(err => console.log('err : ', err));
+          pageNo: 1,
+        },
+      }
+    )
+    .then(async (res) => {
+      return res.data.response.body.item;
+    })
+    .catch((err) => console.log("err : ", err));
 }
